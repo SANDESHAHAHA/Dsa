c22e102c0ffac495d2dc98437db803c3d0fcde41
SANDESHAHAHA <sandeshguragain516@gmail.com>
2025-06-09 23:18:01 +0545

    simple postfix by python

--- lab1/post.py
+++ lab1/post.py
@@ -1,7 +1,20 @@
 def postfix(exp):
     stack=[]
-    exp=input(print("Enter the string: "))
-    exp.split(exp," ")
-    for i in exp:
-        if exp[i]
-
+    operators=['+', '-', '*', '/', '^']
+    tokens=exp.split()
+    for token in tokens:
+        if token not in operators:
+            stack.append(float(token))
+        else:
+                op1=stack.pop()
+                op2=stack.pop()
+                if token=='+':result=op2+op1
+                elif token=='-':result=op2-op1
+                elif token=='*':result=op2*op1
+                elif token=='/':result=op2/op1
+                elif token=='^':result=pow(op2,op1)
+                stack.append(result)
+    return stack[0]
+exp=input("Enter the string: ")
+answer=postfix(exp)
+print(f"The evaluated postfix expression is {answer}")
