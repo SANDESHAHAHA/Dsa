d008bdfc160c9bd514648bbbc69d28b2a0a1d354
SANDESHAHAHA <sandeshguragain516@gmail.com>
2025-08-05 21:58:00 +0545

    radix sort

---
+++ sorting_algo/radixsort.py
@@ -0,0 +1,29 @@
+import random 
+import pprint
+def radixsort(data):
+    radixarray=[[] for _ in range(10)]
+    max_value=max(data)
+    length=len(str(max_value))
+    divisor=1
+    start=1
+    while start <= length:
+        
+        for number in data:
+            index=(number//divisor)%10
+            radixarray[index].append(number)
+        data.clear()
+        for innerarray in radixarray:
+            while innerarray:
+                data.append(innerarray.pop(0))
+        
+        start+=1
+        divisor*=10
+def display(data):
+    print(*data)
+            
+n=int(input("Enter the number of data: "))
+data=[random.randint(0,100) for _ in range(n)]
+display(data)
+print("\n")
+radixsort(data)
+display(data)
